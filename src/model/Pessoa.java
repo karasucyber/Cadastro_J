03e01c9922d0114623efd713635aa40545978943
aquillae7 <aquillae777@gmail.com>
2023-09-18 13:53:34 -0300

    brinca muito

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -6,46 +6,31 @@ public abstract class Pessoa implements Serializable {
     int idade;
     String nome;
     int id;
-
     public Pessoa(int idade, String nome, int id){
         this.idade = idade;
         this.nome = nome;
         this.id = id;
 
-    }
-
-    public String toString (){
+    }public String toString (){
         return "Objeto:" + "\n\t- Classe: " + getClass().getName() + "\n\t- Hash: " + Integer.toHexString(hashCode()) + "\n\t- Nome: " + nome ;
     }
-
     public int getIdade(){
         return idade;
     }
-
     public void setIdade(int Idade){
         this.idade = idade;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome){
         this.nome = nome;
     }
-
     public int getId(){
         return id;
     }
-
-
-
-
     public final void atualizarNome(){
      return ;
 
-
     }
-
-
 }
--- src/model/PessoaJuridicaRepo.java
+++ src/model/PessoaJuridicaRepo.java
@@ -1,59 +1,41 @@
 package model;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 public class PessoaJuridicaRepo {
     private List<PessoaJuridica> jurica;
-
     public PessoaJuridicaRepo(){ jurica = new ArrayList<>();}
-
     public void inserir(PessoaJuridica pessoa){ jurica.add(pessoa);}
-
     public void alterar(PessoaJuridica pessoaAntiga, PessoaJuridica pessoaNova){
         int index = jurica.indexOf(pessoaAntiga);
         if(index != -1){
             jurica.set(index, pessoaNova);
         }
-
     }
-
     public void remove(PessoaJuridica pessoa){ jurica.remove(pessoa);}
-
-     public PessoaJuridica obter(int id){
+    public PessoaJuridica obter(int id){
         for(PessoaJuridica pessoa : jurica){
          if(pessoa.getId() == id){ return pessoa; }}return null;}
-
-
     public List<PessoaJuridica> obterTodos(){
         return new ArrayList<>(jurica);
-    }
-
-
-    public void persistir(String nomeArquivo) {
+    }public void persistir(String nomeArquivo) {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(nomeArquivo))) {
             outputStream.writeObject(jurica);
             System.out.println("Dados persistidos no arquivo: " + nomeArquivo);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void recuperar(String nomeArquivo) {
+    }public void recuperar(String nomeArquivo) {
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(nomeArquivo))) {
             jurica = (List<PessoaJuridica>) inputStream.readObject();
             System.out.println("Dados de Pessoa Jurídica recuperados do arquivo: " + nomeArquivo);
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-    }
-
-    public void imprimirTodos(){
+    }public void imprimirTodos(){
         for(PessoaJuridica pessoa : jurica){
             System.out.println(pessoa);
         }
     }
-
-    // Restante do código da classe
 }
 
